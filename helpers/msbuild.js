df10a1ba404e529d5196f188c8c97a7e6ef184c6
Nestor <nestor.lobo@gmail.com>
2014-07-01 16:08:21 -0300

    Fixed to skip solutios folders, existing or not. Modified sample solution to include both solution folders cases.

--- helpers/msbuild.js
+++ helpers/msbuild.js
@@ -2,14 +2,23 @@ var fs = require('fs'),
     path = require('path'),
     regex = require('./regex.js');
 
+var notExistOrDirectory = function (solutionDirectory, projectPath) {
+    // Return true if the item is not existing or it is a directory
+    var combined = path.normalize(path.join(solutionDirectory, projectPath));
+	if (fs.existsSync(combined))
+	    return fs.statSync(combined).isDirectory();
+	return true;
+};
 
 exports.getSolutionProjects = function(solution) {
     if (!fs.existsSync(solution)) throw new Error('Solution file not found: ' + path.resolve(solution));
     var projectRegEx = /Project\s*\(\s*\".*?\"\s*\)\s*=\s*\".*?\"\s*,\s*\"(.*?)\"\s*,\s*\".*?\"/ig;
     var solutionDirectory = path.dirname(solution);
     return regex.matchAll(projectRegEx, fs.readFileSync(solution, 'utf8')).
-        map(function(projectPath) { return projectPath.replace(/\\/g, path.sep); }).
-        map(function(projectPath) { return path.normalize(path.join(solutionDirectory, projectPath)); });
+        map(function (projectPath) { return projectPath.replace(/\\/g, path.sep); }).
+        // Fix to filter solution folders, existing or not
+        filter(function (projectPath) { return !notExistOrDirectory(solutionDirectory, projectPath); }).
+        map(function (projectPath) { return path.normalize(path.join(solutionDirectory, projectPath)); });
 };
 
 exports.getProjectFiles = function(project, filename) {
